9bc733fb2b0121f4639ef01212af49fb90af05ef
Paulo Seares <athaneduc@gmail.com>
2024-09-25 09:38:33 -0700

    Chapter 13

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -141,7 +141,10 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  
   try {
+
+
     const data = await sql<InvoiceForm>`
       SELECT
         invoices.id,
